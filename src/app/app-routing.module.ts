eb481559d920f2c6f0abd0566c847c52db294e69
Shashank Shekhar <sunnyrocks1122@gmail.com>
2024-08-06 23:25:55 +0530

    Update for t&c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './_components/home/home.component';
 import { PricingComponent } from './_components/pricing/pricing.component';
 import { PrivacyPolicyComponent } from './_components/privacy-policy/privacy-policy.component';
 import { JobsComponent } from './_components/jobs/jobs.component';
+import { TcComponent } from './_components/tc/tc.component';
 
 const routes: Routes = [
   {
@@ -25,6 +26,11 @@ const routes: Routes = [
     path:'privacy-policy',
     component:PrivacyPolicyComponent,
     title:'Privacy Policy'
+  },
+  {
+    path:'terms-&-conditions',
+    component:TcComponent,
+    title:'Terms & Condition'
   }
 ];
 
